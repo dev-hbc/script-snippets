e3f8020a3857009f057a4eb3bbb98261027fe8ff
Hongbin <58645233+dev-hbc@users.noreply.github.com>
2021-01-17 15:25:58 -0500

    x

--- init-general-for-pyex.sh
+++ init-general-for-pyex.sh
@@ -1,8 +1,8 @@
 sudo yum update -y
 sudo yum install -y tmux, vim, wget, git, gcc, python3, python3-pip, python3-devel, python-devel
-pip3 install virtualenv
-pip3 install virtualenvwrapper
-pip3 install uwsgi
+sudo pip3 install virtualenv
+sudo pip3 install virtualenvwrapper
+sudo pip3 install uwsgi
 wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm
 sudo yum install -y epel-release
@@ -11,4 +11,4 @@ sudo systemctl start mysqld
 sudo systemctl start nginx
 
 echo "set -g mode-mouse on" > ~/.tmux.conf
-tmux source-file ~/.tmux.conf
+# tmux source-file ~/.tmux.conf
